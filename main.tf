169d066613da8c010cebbef36a85108028a781d3
Tim Wakefield <twakefield@zonedigital.com>
2023-07-03 15:04:11 +0100

    move cloud

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+    cloud { # The name of your Terraform Cloud organization.
+    organization = "zonedigital"
+
+   # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "probable-couscous"
+    }
+    
+}
   }
 
   required_version = ">= 1.1.0"
